6ccd955cba1371cdb3687e0dae0892977b0fa050
Patrick Lorenzut <patrick.lorenzut@gmail.com>
2019-11-10 18:48:16 -0600

    allow non-auth routes

--- modules/data/verbs.js
+++ modules/data/verbs.js
@@ -4,14 +4,15 @@ window.$invoke = require('~/modules/app')
 export async function getVerb(spanish) {
     return await $invoke._invoker(
         'get',
-        process.env.baseApiUrl + '/verbs/' + spanish
+        process.env.baseApiUrl + '/verbs/' + spanish,
+        {},{},false
     )
 }
 
 export async function getAnswers() {
     return await $invoke._invoker(
         'get',
-        process.env.baseApiUrl + '/answers/me?_with=conjugation&_sort=-created_at&_limit=50'
+        process.env.baseApiUrl + '/answers/me?_with=conjugation&_sort=-created_at&_limit=50'        
     )
 }
 
@@ -26,20 +27,23 @@ export async function recordAnswer(formData) {
 export async function getQuestions(tenses, persons, regulars, singleVerb) {
     return await $invoke._invoker(
         'get',
-        process.env.baseApiUrl + '/questions?_with=verb,tense.mood,person&tenses_id-in=' + tenses + '&persons_id-in=' + persons + '&is_irregular-in=' + regulars + (singleVerb && singleVerb.id ? ('&verbs_id=' + singleVerb.id) : '')
+        process.env.baseApiUrl + '/questions?_with=verb,tense.mood,person&tenses_id-in=' + tenses + '&persons_id-in=' + persons + '&is_irregular-in=' + regulars + (singleVerb && singleVerb.id ? ('&verbs_id=' + singleVerb.id) : ''),
+        {},{},false
     )
 }
 
 export async function getMoods() {
     return await $invoke._invoker(
         'get',
-        process.env.baseApiUrl + '/moods?_with=tenses&id-in=1,4,5'
+        process.env.baseApiUrl + '/moods?_with=tenses&id-in=1,4,5',
+        {},{},false
     )
 }
 
 export async function getPersons() {
     return await $invoke._invoker(
         'get',
-        process.env.baseApiUrl + '/persons'
+        process.env.baseApiUrl + '/persons',
+        {},{},false
     )
 }
